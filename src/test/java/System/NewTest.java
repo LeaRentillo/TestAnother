5d2ed77a76299a58d03198c6501b66249dc2df7f
Dell <Dell@192.168.3.87>
2018-07-30 17:11:56 +0800

    test

--- src/test/java/System/NewTest.java
+++ src/test/java/System/NewTest.java
@@ -9,15 +9,17 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 
 public class NewTest {
-	private ChromeDriver driver;
+	public WebDriver driver;
 	public static final String USERNAME = "G00003@100002.com";
 	public static final String USERNAME1 = "G00003@100002.com2";
 	public static final String ACCESS_KEY = "nQr6n6Td!9";
 
-	@Test
+	@Test (priority = 1, alwaysRun = true)
 	public void f() {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\Dell\\Documents\\LEA\\SELENIUM\\chromedriver_win32\\chromedriver.exe");
@@ -27,7 +29,7 @@ public class NewTest {
 
 		WebElement usernameElement = driver.findElement(By.name("LoginForm[login]"));
 		WebElement passwordElement = driver.findElement(By.name("LoginForm[password]"));
-		WebElement formElement = driver.findElementById("LoginForm");
+		WebElement formElement = driver.findElement(By.id("LoginForm"));
 
 		usernameElement.sendKeys(USERNAME);
 		passwordElement.sendKeys(ACCESS_KEY);
